5cca67f9eb6f2f286eda14bf4b866f6c7bd864e2
Perlik <mp429581@students.mimuw.edu.pl>
2022-03-27 19:08:09 +0200

    more comments

--- polynomial_degree.asm
+++ polynomial_degree.asm
@@ -67,30 +67,26 @@ move_array_next_step: ; przesuwamy wskaźniki do następnych pozycji i powtarzam
     pop rcx
     loop move_array
 
+    ; etap ustawiania początkowych wartości w tablicy zakończony
+    ; teraz będziemy w pętli odejmowali sąsiedznie liczby w tablicy, w każdym kroku uznając, że jej rozmiar zmniejsza się o jedną liczbę. 
+
     mov r9, [rel array_size_bytes] ; aktualny rozmiar tablicy, w kolejnych iteracjach będzie się zmniejszać o tyle, ile miejsca zajmuje jedna liczba w tablicy, tj. number_of_segments
 
     ; w r11 jest aktualnie number_of_segments, więc 8 * r11 jest wartością, o którą trzeba przesunąć wskaźnik, by dostać się do kolejnej liczby w tablicy
     lea r10, [r9 + 8 * r11]
 
-    mov rcx, r9
     mov rsi, -1 ; w rsi będzie trzymany wynikowy stopień wielomianu
-    mov rdi, [rel n]
+    mov rdi, [rel n] ; w rdi będzie trzymany aktualny rozmiar tablicy, tzn. liczba liczb, które mają w niej swoją reprezentację
 
-    mov rax, rdi
-    mul QWORD [rel number_of_segments]
-    mov rcx, rax
+    mov rax, rsp ; ustawiamy rax na początek tablicy ze stosu
+    mov rcx, [rel array_size_bytes]
 
-    mov rax, rsp
-
-    cmp rcx, 0
-    je end
-
-    jmp check_zeros_array
+    jmp check_zeros_array ; na początek chcemy sprawdzić, czy tablica zawiera same zera
 
 subtract:
     push rcx
-    mov rcx, [rel number_of_segments]
-    clc
+    mov rcx, [rel number_of_segments] ; dla dwóch liczb trzeba odjąć wszystkie odpowiadające segmenty, z których się składają
+    clc ; nie interesuje nas flaga przeniesienia z poprzedniej operacji arytmetycznej
 
 subtract_two:
     mov rdx, [r10]
@@ -106,22 +102,25 @@ subtract_two:
     mov rax, rsp
     mov rcx, r9
     
-check_zeros_array:
+check_zeros_array: ; sprawdza w pętli, czy tablica składa się z samych zer
     cmp QWORD [rax], 0
     jne non_zero
     lea rax, [rax + 8]
     loop check_zeros_array
     jmp end
 
-non_zero:
-    sub r9, [rel number_of_segments]
-    sub rdi, 1
-    mov rcx, rdi
-    lea rsi, [rsi + 1]
-    cmp rcx, 0
+non_zero: ; tablica zawiera niezerowy element, trzeba odjąć sąsiednie liczby w tablicy
+    sub r9, [rel number_of_segments] ; liczba segmentów w wynikowej tablicy zmniejszy się o number_of_segments
+    sub rdi, 1 ; rozmiar wynikowej tablicy po odejmowaniu zmniejszy się o jeden
+
+    mov rcx, rdi ; będzie potrzeba wykonać rdi odejmowań liczb
+    lea rsi, [rsi + 1] ; wynikowy stopień wielomianu zwiększamy o jeden
+
+    cmp rcx, 0 ; jeśli rozmiar aktualnej tablicy wynosi 1, to rcx jest zerem i kończymy program z aktualnym rsi
     je end
-    mov rax, rsp
-    lea r10, [rax + 8 * r11]
+
+    mov rax, rsp ; wracamy na początek tablicy w rax
+    lea r10, [rax + 8 * r11] ; wskaźnik na sąsiednią liczbę w tablicy
     jmp subtract
 
 end:
